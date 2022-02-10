14944474692af4f62fc571d013a04b036d548e7d
snaseirm <93827194+snaseirm@users.noreply.github.com>
2022-02-10 20:53:08 +0100

    Update mutations.js

--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -6,7 +6,26 @@ export default {
 		state.coaches = state.coaches.filter(obj => obj.id != payload);
 	},
 	editCoach(state, payload) {
-		state.coaches.put(payload);
+		console.debug(typeof payload);
+
+		if (!Array.isArray(state.coaches)) {
+			state.coaches = [];
+		}
+
+		var original = state.coaches.find(obj => obj.id === payload.id);
+		let index = state.coaches.findIndex(q => q.id === payload.id);
+
+		if(original == null || original === -1)
+		{
+			state.coaches.push(payload);
+		}
+		else {
+
+			let temp = state.coaches;
+			temp[index] = payload;
+			state.coaches = temp;
+		}
+
 	},
 	setCoaches(state, payload) {
 		state.coaches = payload;
